473ebf83d62ac8cfe45da27c4a30aaf5ee593823
h4j4x <miche.atucha@gmail.com>
2022-07-25 16:29:22 -0500

    grokking The Longest Substring with maximum K Distinct characters

--- kotlin/src/main/kotlin/com/h4j4x/algos/kotlin/grokking/SubArray.kt
+++ kotlin/src/main/kotlin/com/h4j4x/algos/kotlin/grokking/SubArray.kt
@@ -3,20 +3,44 @@ package com.h4j4x.algos.kotlin.grokking
 object SubArray {
     fun smallestSubarrayWithGreaterSum(arr: IntArray, sum: Int): IntArray {
         var maxFrom = 0
-        var maxTo = arr.lastIndex
+        var maxTo = arr.size
         var from = 0
-        var fromToSum = 0
+        var indexSum = 0
         for (index in arr.indices) {
-            fromToSum += arr[index]
-            while (fromToSum >= sum) {
-                if (index - from <= maxTo - maxFrom) {
+            indexSum += arr[index]
+            while (indexSum >= sum) {
+                if (index - from <= maxTo - maxFrom && indexSum >= sum) {
                     maxFrom = from
                     maxTo = index
                 }
-                fromToSum -= arr[from]
+                indexSum -= arr[from]
                 from++
             }
         }
-        return IntArray(maxTo - maxFrom + 1) { arr[it + maxFrom] }
+        return arr.copyOfRange(maxFrom, maxTo + 1)
+    }
+
+    fun longestSubstringWithMaxKDistinctChars(str: String, distinctChars: Int): String {
+        var from = 0
+        var maxFrom = 0
+        var maxTo = 0
+        val countMap = mutableMapOf<Char, Int>()
+        for (index in str.indices) {
+            val char = str[index]
+            countMap[char] = (countMap[char] ?: 0) + 1
+            while (countMap.size > distinctChars) {
+                val fromChar = str[from]
+                countMap[fromChar] = countMap[fromChar]!! - 1
+                if (countMap[fromChar] == 0) {
+                    countMap.remove(fromChar)
+                }
+                from++
+            }
+            if (index - from >= maxTo - maxFrom) {
+                maxFrom = from
+                maxTo = index
+            }
+        }
+        return str.substring(maxFrom, maxTo + 1)
     }
 }
--- kotlin/src/test/kotlin/com/h4j4x/algos/kotlin/grokking/SubArrayTests.kt
+++ kotlin/src/test/kotlin/com/h4j4x/algos/kotlin/grokking/SubArrayTests.kt
@@ -1,6 +1,7 @@
 package com.h4j4x.algos.kotlin.grokking
 
 import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class SubArrayTests {
@@ -14,5 +15,20 @@ class SubArrayTests {
 
         subarray = SubArray.smallestSubarrayWithGreaterSum(intArrayOf(3, 4, 1, 1, 6), 8)
         assertArrayEquals(intArrayOf(1, 1, 6), subarray)
+    }
+
+    @Test
+    fun testLongestSubstringWithMaxKDistinctChars() {
+        var subString = SubArray.longestSubstringWithMaxKDistinctChars("araaci", 2)
+        assertEquals("araa", subString)
+
+        subString = SubArray.longestSubstringWithMaxKDistinctChars("araaci", 1)
+        assertEquals("aa", subString)
+
+        subString = SubArray.longestSubstringWithMaxKDistinctChars("cbbebi", 3)
+        assertEquals("bbebi", subString)
+
+        subString = SubArray.longestSubstringWithMaxKDistinctChars("cbbebi", 10)
+        assertEquals("cbbebi", subString)
     }
 }
